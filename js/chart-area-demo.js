c87fc4f04da611192a7956f058c5af4e6b727da2
logicOnAbstractions <logiconabstractions@gmail.com>
2021-12-23 14:19:03 -0500

    Decent looking simple demo

--- js/chart-area-demo.js
+++ js/chart-area-demo.js
@@ -4,6 +4,8 @@
 Chart.defaults.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.defaultFontColor = '#858796';
 
+let num_stations = 6;
+
 function number_format(number, decimals, dec_point, thousands_sep) {
   // *     example: number_format(1234.56, 2, ',', ' ');
   // *     return: '1 234,56'
@@ -37,6 +39,28 @@ function random_thickness(offset, range=2) {
 // Datetimes to display on chart
 let datetimes = [];
 let ice_thick = [];
+
+// wrapper to generate each graph with data independantly
+function generate_random_thickness_dates() {
+  let ice_thick_array = [];
+  let random_offset = Math.random()+0.5
+  for (let i = 1; i < 30; i++) {
+    let offset;
+    let range;
+    datetimes.push(luxon.DateTime.utc(2021, 12, i, 12, 30));
+    if (i>20){
+      offset = 3*random_offset + random_offset;
+      range = 1;
+    } else {
+      offset = i/5 + random_offset;
+      range = 2
+    }
+    ice_thick_array.push(random_thickness(offset, range));
+  }
+
+  return ice_thick_array
+}
+
 for (let i = 1; i < 30; i++) {
   let offset;
   let range;
@@ -57,35 +81,55 @@ datetimes.forEach(function(item, index, array) {
   datetimes_isos.push(item.toISO());
 });
 
-// Chart's data
-// let data = [{ x: datetimes_isos[0], y: 0 }, { x: datetimes_isos[1], y: 10 }, { x: datetimes_isos[2], y: 20 }, { x: datetimes_isos[3], y: 40 }, { x: datetimes_isos[4], y: 50 }, { x: datetimes_isos[5], y: 60 }];
-let data = [];
-for (let i = 0; i < datetimes.length; i++) {
-  data.push({x:datetimes[i], y:ice_thick[i]})
+
+function get_dummy_data(){
+  let data = [];
+  let thickness = generate_random_thickness_dates()
+  for (let i = 0; i < datetimes.length; i++) {
+    data.push({x:datetimes[i], y:thickness[i]})
+  }
+  return {labels: datetimes_isos, datasets: [{data: data}]}
 }
 
-const chartData = {
-  labels: datetimes_isos,
-  datasets: [{ data: data }]
-};
 
-const config = {
-  type: 'line',
-  data: chartData,
-  options: {
-    scales: {
-      x: {
-        type: 'time',
-        ticks:{
-          source:'data'
+
+function build_configs(){
+  let config = {
+    type: 'line',
+    data: get_dummy_data(),
+    options: {
+      plugins: {
+        legend: {display: false}
+      },
+      scales: {
+        x: {
+          title: {
+            text: "Date (or relevent time intervale)",
+            display: true
+          },
+          type: 'time',
+          ticks:{
+            source:'data'
+          },
+          time: {
+            unit: "day"
+          },
         },
-        time: {
-          unit: "day"
-        },
-      }
+
+        y: {
+          title: {
+            text: "Thickness (inches)",
+            display: true
+          }
+        }
+      },
     },
-  },
-};
+  };
+  return config
+}
+
+
+
 let ctx1 = $("#ex1")
 let ctx2 = $("#ex2")
 let ctx3 = $("#ex3")
@@ -93,9 +137,12 @@ let ctx4 = $("#ex4")
 let ctx5 = $("#ex5")
 let ctx6 = $("#ex6")
 
-var theChart1 = new Chart(ctx1, config);
-var theChart2 = new Chart(ctx2, config);
-var theChart3 = new Chart(ctx3, config);
-var theChart4 = new Chart(ctx4, config);
-var theChart5 = new Chart(ctx5, config);
-var theChart6 = new Chart(ctx6, config);
+
+
+
+var theChart1 = new Chart(ctx1, build_configs());
+var theChart2 = new Chart(ctx2, build_configs());
+var theChart3 = new Chart(ctx3, build_configs());
+var theChart4 = new Chart(ctx4, build_configs());
+var theChart5 = new Chart(ctx5, build_configs());
+var theChart6 = new Chart(ctx6, build_configs());
---
+++ js/main.js
@@ -0,0 +1,5 @@
+$('.dropdown-item').on('click',  function(){
+    var btnObj = $(this).parent().siblings('button');
+    $(btnObj).text($(this).text());
+    $(btnObj).val($(this).text());
+});
